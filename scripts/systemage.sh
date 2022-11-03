f84862898a3178cbe9889718caeae145fdf5c2e5
nebulosa2007 <nebulosa2007@yandex.ru>
2022-11-03 17:04:14 +0300

    age script fixes

--- scripts/systemage.sh
+++ scripts/systemage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-METHOD1=$(ls -ctl --time-style +"%Y-%m-%d" /etc | tail -1 | cut -d" " -f9)
-#METHOD2=$(head -1 /var/log/pacman.log | cut -c 2-11)
+METHOD=$(/usr/bin/ls -ctl --time-style +"%Y-%m-%d" /etc | tail -1 | cut -d" " -f9)
+#METHOD=$(head -1 /var/log/pacman.log | cut -c 2-11)
 
-DAYSBETWEEN=$(( ($(date -d $(date +%Y-%m-%d) +%s) - $(date -d $METHOD1 +%s)) / 86400 ))
+DAYSBETWEEN=$(( ($(date -d $(date +%Y-%m-%d) +%s) - $(date -d $METHOD +%s)) / 86400 ))
 
 YEARSCALC=$(( $DAYSBETWEEN / 365 ))
 MONTHSCALC=$(( ($DAYSBETWEEN - ($YEARSCALC * 365)) / 30 ))
@@ -13,4 +13,4 @@ DAYSCALC=$(( $DAYSBETWEEN -($YEARSCALC * 365) - ($MONTHSCALC * 30) ))
 [[ $MONTHSCALC > 0 ]] && MONTHS=$MONTHSCALC"m "
 [[ $DAYSCALC > 0   ]] && DAYS=$DAYSCALC"d"
 
-echo "System age: "$YEARS$MONTHS$DAYS" (since $METHOD1)"
+echo "System age: "$YEARS$MONTHS$DAYS" (since $METHOD)"
