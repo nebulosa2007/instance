5dfffd0e286b947f55dcde67232de17668ae6211
nebulosa2007 <nebulosa2007@yandex.ru>
2022-11-14 13:53:20 +0300

    install instance script updated

--- instance_install.sh
+++ instance_install.sh
@@ -124,14 +124,14 @@ pikaur -Syu --needed iproute2 lsd netcat micro mc reflector htop btop expac neof
 # instance project installing
 cd
 git clone git@github.com:nebulosa2007/instance.git
-ln -s /home/$(whoami)/instance/bashrc .bashrc
+ln -sf /home/$(whoami)/instance/bashrc .bashrc
 ln -s /home/$(whoami)/instance/bash_aliases .bash_aliases
 sudo ln -s /home/$(whoami)/instance/etc/tmux.conf /etc/tmux.conf
 sudo cp /home/$(whoami)/instance/etc/99-sysctl.conf /etc/sysctl.d/99-sysctl.conf
 sudo sysctl --system
 
 # install update timer
-cd update
+cd instance/update
 sudo ln -s /home/$(whoami)/instance/update/updpkgs.sh /usr/bin/updpkgs
 sudo ln -s /home/$(whoami)/instance/update/update.service /lib/systemd/system/update.service
 sudo ln -s /home/$(whoami)/instance/update/update.timer /lib/systemd/system/update.timer
@@ -140,7 +140,7 @@ sudo systemctl enable --now update.timer
 
 # install localepurge
 pikaur -Syu --needed localepurge
-sudo ln -s /home/$(whoami)/instance/etc/locale.nopurge /etc/locale.nopurge
+sudo ln -sf /home/$(whoami)/instance/etc/locale.nopurge /etc/locale.nopurge
 
 
 # install wireguard
