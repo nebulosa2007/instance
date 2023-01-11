0ccecfe4a0c9083a3210759f6d776c1d920cc79b
nebulosa2007 <85841412+nebulosa2007@users.noreply.github.com>
2023-01-11 20:38:06 +0300

    Update vps_install.sh

--- vps_install.sh
+++ vps_install.sh
@@ -1,7 +1,7 @@
 # Tuning network
 
 #For DCHP ONLY!
-#printf "[Match]\nName=en*\n\n[Network]\nDHCP=yes\n" > /etc/systemd/network/20-wired.network
+#printf "[Match]\nName=en*\nName=eth*\n\n[Network]\nDHCP=yes\n" > /etc/systemd/network/20-ethernet.network
 # or
 ## /etc/systemd/network/20-ethernet.network
 ## Name=en*
@@ -16,9 +16,10 @@ ip -c a
 
 passwd
 
+
+### throught ssh connection
 lsblk
 
-# throught ssh connection
 # BIOS machine: dos, primary, bootable
 cfdisk /dev/vda
 
@@ -27,14 +28,15 @@ tune2fs -m1 -r1 /dev/vda1
 
 mount -o defaults,noatime,commit=60 /dev/vda1 /mnt
 lsblk
+
+#Pacman tuning
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
+sed -i 's/#NoExtract   =/NoExtract   = usr\/share\/man\/* usr\/share\/help\/* usr\/share\/locale\/* !usr\/share\/locale\/en_US* !usr\/share\/locale\/locale.alias/' /etc/pacman.conf
 
-pacstrap /mnt base base-devel linux grub e2fsprogs
+pacstrap /mnt base linux grub e2fsprogs
 
-cp -f /etc/systemd/network/20-ethernet.network /mnt/etc/systemd/network/20-ethernet.network
-cp -f /etc/resolv.conf /mnt/etc/resolv.conf
-sudo systemctl enable systemd-networkd
-
+cp -i /etc/systemd/network/20-ethernet.network /mnt/etc/systemd/network/20-ethernet.network
+cp -i /etc/pacman.conf /mnt/etc/pacman.conf
 
 #generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -42,9 +44,11 @@ genfstab -U /mnt >> /mnt/etc/fstab
 #start the party!
 arch-chroot /mnt
 
-
 ##INSIDE CHROOT
 cat /etc/fstab
+
+#Time tuning
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime && hwclock --systohc
 
 #for default preset only in /etc/mkinitcpio.d/linux.preset :  PRESETS=('default')
 sed -i "s/PRESETS=('default' 'fallback')/PRESETS=('default')/" /etc/mkinitcpio.d/linux.preset
@@ -56,9 +60,9 @@ grub-install --target=i386-pc --recheck /dev/vda
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
-sed -i 's/#NoExtract   =/NoExtract   = usr\/share\/man\/* usr\/share\/help\/* usr\/share\/locale\/* !usr\/share\/locale\/en_US* !usr\/share\/locale\/locale.alias/' /etc/pacman.conf
-
+#Network tuning
+systemctl enable systemd-networkd
+echo "nameserver 9.9.9.9" > /etc/resolv.conf
 
 #set variables
 M=vps
@@ -69,7 +73,7 @@ sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen && locale-gen
 #Set locales for other GUI programs
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-#Set machine name. "virtarch" in my case.
+#Set machine name. "vps" in my case.
 echo $M >> /etc/hostname
 printf "127.0.0.1 localhost\n::1       localhost\n127.0.0.1 $M.localhost $M\n" >> /etc/hosts
 cat /etc/hostname /etc/hosts
@@ -83,6 +87,12 @@ passwd $U
 #Sudo activating
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
+#Zram
+echo "zram" > /etc/modules-load.d/zram.conf
+echo "options zram num_devices=1" > /etc/modprobe.d/zram.conf
+echo 'KERNEL=="zram0", ATTR{disksize}="'$(awk '/MemTotal/ {print $2}' /proc/meminfo)'" RUN="/usr/bin/mkswap /dev/zram0", TAG+="systemd"' > /etc/udev/rules.d/99-zram.rules
+echo "/dev/zram0 none swap defaults 0 0" >> /etc/fstab
+
 pacman -S openssh
 systemctl enable sshd
 
@@ -92,7 +102,8 @@ systemctl enable fstrim.timer
 pacman -S vnstat
 systemctl enable vnstat
 
-exit&&cd&&umount -R /mnt
+exit
+umount -R /mnt
 reboot
 
 # Login trought ssh, then:
@@ -102,27 +113,21 @@ reboot
 sudo sed -i 's/#PasswordAuthentication no/PasswordAuthentication no/' /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
-#Tune date and time. 'timedatectl list-timezones' for other variants
-sudo timedatectl set-timezone Europe/Moscow
+#Tune NTP
 sudo timedatectl set-ntp true
 sudo timedatectl status
 
 #reflector
 sudo pacman -S reflector
-sudo reflector --verbose -l 3 -p https --sort rate --save /etc/pacman.d/mirrorlist
 sudo systemctl enable reflector.timer
 
 #pikaur - AUR helper, smallest one
-sudo pacman -S --needed git
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/pikaur.git
 cd pikaur && makepkg -fsri && cd .. && rm -rf pikaur
 
-#Install zramd
-pikaur -S zramd
-sudo systemctl enable --now zramd
-
 #Install usefull soft
-pikaur -S htop neofetch ranger
+pikaur -S htop neofetch ranger mc micro ncdu
 
 # install wireguard
 cd && mkdir wireguard && cd wireguard
