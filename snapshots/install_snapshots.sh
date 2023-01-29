317dce890668f5de989e9aeb7fee711bda1671e0
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-01-29 16:49:39 +0300

    Update snapshot install: clean cache

--- snapshots/install_snapshots.sh
+++ snapshots/install_snapshots.sh
@@ -3,3 +3,7 @@
 sudo mkdir -p /etc/pacman.d/hooks
 sudo sudo ln -s /home/$(whoami)/instance/snapshots/snaproot.sh /usr/bin/snaproot
 sudo cp /home/$(whoami)/instance/snapshots/01-btrfs-autosnap.hook /etc/pacman.d/hooks/01-btrfs-autosnap.hook
+
+#Cache cleaning for minimize size of snapshots
+sudo rm -f /var/cache/pacman/pkg/*
+sudo sed -i 's/#CacheDir    = \/var\/cache\/pacman\/pkg\//CacheDir     = \/tmp\//' /etc/pacman.conf
