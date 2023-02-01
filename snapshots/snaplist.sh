a7162113c0ed3aa98ce71a4b767f9f5eb759b7ee
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-02-01 17:12:40 +0300

    Update snaplist -U and downgraded operations

--- snapshots/snaplist.sh
+++ snapshots/snaplist.sh
@@ -23,10 +23,12 @@ do
             [ $operation == "--sync" ] && echo -ne ${green}"Before installing  "
             [ $operation == "-Rsc" ] && echo -ne ${red}"Before deleting    "
             [ $operation == "-Rsnc" ] && echo -ne ${red}"Before deleting    "
+            [ $operation == "-Rsn" ] && echo -ne ${red}"Before deleting    "
             [ $operation == "--remove" ] && echo -ne ${red}"Before deleting    "
             [ $operation == "starting" ] && echo -ne ${yellow}"Before upgrading   "
+            [ $operation == "-U" ] && echo -ne ${yellow}"Before upgrading   "
             updated=$(awk '/'$snapshot'/,/transaction completed$/' /var/log/pacman.log | grep -E "\[ALPM\] upgraded" | cut -d" " -f4,7 | sed 's/)/,/')
-            [ "$updated" == "" ] && updated=$(awk '/'$snapshot'/,/transaction completed$/' /var/log/pacman.log | grep -E "\[ALPM\] (removed|installed)" | cut -d" " -f4,5,7 | sed 's/)/,/;s/(//') && pacman_log_line=" "
+            [ "$updated" == "" ] && updated=$(awk '/'$snapshot'/,/transaction completed$/' /var/log/pacman.log | grep -E "\[ALPM\] (removed|installed|downgraded)" | cut -d" " -f4,5,7 | sed 's/)/,/;s/(//') && pacman_log_line=" "
             updated=$(echo $updated | sed 's/,$/ /;s/(/ /')
             echo -e $pacman_log_line$updated${nc}
         fi
