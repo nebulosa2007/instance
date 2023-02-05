c0effbf10b1c05d212ffee0d7d8050db07465c24
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-02-05 11:42:48 +0300

    Update system age method detection

--- scripts/systemage.sh
+++ scripts/systemage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-METHOD=$(/usr/bin/ls -ctl --time-style +"%Y-%m-%d" /etc | tail -1 | sed 's/ /\n/g'| tail -2 | head -1)
+METHOD=$(stat /etc | tail -1 | cut -d" " -f3)
+#METHOD=$(/usr/bin/ls -ctl --time-style +"%Y-%m-%d" /etc | tail -1 | sed 's/ /\n/g'| tail -2 | head -1)
 #METHOD=$(head -1 /var/log/pacman.log | cut -c 2-11)
 
 DAYSBETWEEN=$(( ($(date -d $(date +%Y-%m-%d) +%s) - $(date -d $METHOD +%s)) / 86400 ))
