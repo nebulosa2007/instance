9396fce3c874ed4a455972ac8bcec3a62d280d3c
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-02-10 23:46:44 +0300

    Added empty operators

--- wireguard/wgstat.sh
+++ wireguard/wgstat.sh
@@ -10,6 +10,6 @@ sudo /usr/bin/wg show wg0 | awk '/allowed ips/,/transfer/' |  while read stat_li
 do
 	key_ip=$(echo $stat_line | awk '/allowed ips:/ {print $3}')
 	[ "$key_ip" == "" ] || (echo -e "Client:" $(echo $CLIENTS | tr '|' '\n' | grep $key_ip | cut -d" " -f1))
-	[ "$(echo $stat_line | grep -E '(peer|preshared|allowed)')" == "" ] && echo $stat_line
-	[ "$(echo $stat_line | grep 'transfer')" != "" ] && echo
+	[ "$(echo $stat_line | grep -E '(peer|preshared|allowed)')" == "" ] && echo $stat_line || :
+	[ "$(echo $stat_line | grep 'transfer')" != "" ] && echo || :
 done
