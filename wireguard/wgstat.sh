dadf27b7845481d97175d9e36efb75dd85d2dc6a
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-02-10 21:01:51 +0300

    Some golfcoding

--- wireguard/wgstat.sh
+++ wireguard/wgstat.sh
@@ -9,19 +9,7 @@ echo
 sudo /usr/bin/wg show wg0 | awk '/allowed ips/,/transfer/' |  while read stat_line
 do
 	key_ip=$(echo $stat_line | awk '/allowed ips:/ {print $3}')
-	if [ "$key_ip" == "" ]
-	then
-		client_name=""
-	else
-		client_name=$(echo $CLIENTS | tr '|' '\n' | grep $key_ip | cut -d" " -f1)
-		echo "Client: "$client_name
-	fi
-	if [ "$(echo $stat_line | grep -E '(peer|preshared|allowed)')" == "" ]
-	then
-		echo $stat_line
-	fi
-	if [ "$(echo $stat_line | grep 'transfer')" != "" ]
-	then
-		echo
-	fi
+	[ "$key_ip" == "" ] || (echo -e "Client:" $(echo $CLIENTS | tr '|' '\n' | grep $key_ip | cut -d" " -f1))
+	[ "$(echo $stat_line | grep -E '(peer|preshared|allowed)')" == "" ] && echo $stat_line
+	[ "$(echo $stat_line | grep 'transfer')" != "" ] && echo
 done
