e556c15c5ec45a65b4e1f1789f49332221eae7e9
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-03-07 21:57:35 +0300

    Added days preset

--- wireguard/wgstat.sh
+++ wireguard/wgstat.sh
@@ -9,6 +9,6 @@ sudo /usr/bin/wg show wg0 | awk '/allowed ips/,/transfer/' |  while read stat_li
 do
 	key_ip=$(echo $stat_line | awk '/allowed ips:/ {print $3}')
 	[ "$key_ip" == "" ]  || (echo -ne "\n" $(echo $CLIENTS | tr '|' '\n' | grep $key_ip | cut -d" " -f1 | sed 's/ //g')) 
-	[ "$(echo $stat_line | grep -E '(peer|preshared|allowed)')" == "" ] && (echo -n $stat_line | sed 's/ minute\(s\)*/m/g;s/ hour\(s\)*/h/g;s/ second\(s\)*/s/g;s/, //g;s/transfer: /  ↑/g;s/received/  ↓/g;s/latest handshake:/: /g;s/ago//;s/ sent//g;') || :
+	[ "$(echo $stat_line | grep -E '(peer|preshared|allowed)')" == "" ] && (echo -n $stat_line | sed 's/ day\(s\)*/d/g;s/ minute\(s\)*/m/g;s/ hour\(s\)*/h/g;s/ second\(s\)*/s/g;s/, //g;s/transfer: /  ↑/g;s/received/  ↓/g;s/latest handshake:/: /g;s/ago//;s/ sent//g;') || :
 	[ "$(echo $stat_line | grep 'transfer')" != "" ] && echo || :
 done
