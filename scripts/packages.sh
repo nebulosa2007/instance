d231b79887f0d5d262b7d39f10b6a570908991fc
nebulosa2007 <85841412+nebulosa2007@users.noreply.github.com>
2023-03-10 13:06:20 +0300

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# pikaur -Syu --needed expac
+# pacman -Syu --needed expac
 
 echo -e  "\033[1mFrom repository:\033[0m"
 expac -H M "%011m\t%-20n\t%10d" $(comm -23 <(pacman -Qqen | sort) <((expac -l '\n' '%E' base-devel; expac -l '\n' '%E' base) | sort -u)) 
