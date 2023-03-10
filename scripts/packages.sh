ab69a2e05fcbe230d9e56b19f7f8c77bd40057de
nebulosa2007 <85841412+nebulosa2007@users.noreply.github.com>
2023-03-10 13:12:18 +0300

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# pacman -Syu --needed expac
+# https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#Packages_and_dependencies
+## sudo pacman -Syu --needed expac
 
 echo -e  "\033[1mFrom repository:\033[0m"
 expac -H M "%011m\t%-20n\t%10d" $(comm -23 <(pacman -Qqen | sort) <((expac -l '\n' '%E' base-devel; expac -l '\n' '%E' base) | sort -u)) 
