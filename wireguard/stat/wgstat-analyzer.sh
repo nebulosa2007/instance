c8b6c537876004ca928874a19b7a9089be06d458
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-03-10 19:54:59 +0300

    Refactoring and fixies

--- wireguard/stat/wgstat-analyzer.sh
+++ wireguard/stat/wgstat-analyzer.sh
@@ -2,16 +2,15 @@
 
 function client_daily ()
 {
-	j=0
-	TRDADD=0
-	TRXADD=0
+	j=0; TRDADD=0; TRXADD=0;
+	MINTRX=0; MAXTRX=0;
+	MINTRD=0; MAXTRD=0;
 	OLDIFS=$IFS; IFS=$'\n';
 	for LINE in $(grep $1" " $LOGSDIR/$2*.log)
 	do 
 		IFS=$OLDIFS
 		declare -a arr=($LINE)
 		[ "$j" -eq 0 ] && { MINTRX=${arr[5]}; MINTRD=${arr[6]}; }
-		#echo ${arr[0]} $MINTRX $MINTRD ${arr[2]}
 		if [ "${arr[2]}" -gt 3600 ]
 		then
 			MAXTRX=${arr[5]}
@@ -19,9 +18,8 @@ function client_daily ()
 			let "j = j + 1"
 		else
 			let "TRDADD = TRDADD + MAXTRD - MINTRD"
-			let "TRXADD = TRXADD + MAXTRD - MINTRD"
-			MINTRD=$MAXTRD
-			MINTRX=$MINTRX
+			let "TRXADD = TRXADD + MAXTRX - MINTRX"
+			MINTRX=${arr[5]}; MINTRD=${arr[6]};
 		fi
 	done
 	let "TRDD = TRDADD +  MAXTRD - MINTRD"
@@ -48,16 +46,37 @@ then
 				let "TRDM = TRDM + TRDD"
 				let "TRXM = TRXM + TRXD"
 			done
-			let "TOTALM =  TOTALM + TRDM + TRXM"			
-			echo $MONTH" "$CLIENT" "$((($TRDM+$TRXM)/1024/1024)) 
+			#let "TOTALM =  TOTALM + TRDM + TRXM"			
+			echo $MONTH" "$CLIENT" "$((($TRDM+$TRXM)/1000/1000)) 
 		done
-		echo "Total "$MONTH": "$(($TOTALM/1024/1024))" ("$(($TOTALM/1024/1024/1024))" Gb)"
+		#echo "Total "$MONTH": "$(($TOTALM/1000/1000))" ("$(($TOTALM/1000/1000/1000))" Gb)"
 		echo
 	done
 else
-	for DAY in $DAYS
-	do
-		client_daily $1 $DAY
-		echo $1" "$DAY" TRX: "$(($TRXD/1024/1024))" Mb TRD: "$(($TRDD/1024/1024))" Mb Total: "$((($TRDD+$TRXD)/1024/1024))" Mb"
-	done
+	if [ -z $2 ]
+	then
+		for DAY in $DAYS
+		do
+			client_daily $1 $DAY
+			echo $1" "$DAY" TRX: "$(($TRXD/1000/1000))" Mb TRD: "$(($TRDD/1000/1000))" Mb Total: "$((($TRDD+$TRXD)/1000/1000))" Mb"
+		done
+	else
+		TOTALM=0;
+		if [ "$1" == "All" ] 
+		then
+			for CLIENT in $CLIENTS
+			do
+				TRXM=0;TRDM=0;
+				client_daily $CLIENT $2
+				let "TRDM = TRDM + TRDD"
+				let "TRXM = TRXM + TRXD"
+				#let "TOTALM =  TOTALM + TRDM + TRXM"			
+				echo $2 $CLIENT" "$((($TRDM+$TRXM)/1000/1000)) 
+			done
+		else
+			client_daily $1 $2
+			echo $1" "$2" TRX: "$(($TRXD/1000/1000))" Mb TRD: "$(($TRDD/1000/1000))" Mb Total: "$((($TRDD+$TRXD)/1000/1000))" Mb"
+		fi
+		# echo "Total "$2": "$(($TOTALM/1000/1000))" ("$(($TOTALM/1000/1000/1000))" Gb)"	
+	fi
 fi
