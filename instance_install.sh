71724b6fbdda9a4a8821601ef74844e4432ece73
nebulosa2007 <85841412+nebulosa2007@users.noreply.github.com>
2023-03-15 13:21:58 +0300

    Update instance_install.sh

--- instance_install.sh
+++ instance_install.sh
@@ -1,18 +1,17 @@
-# Install programs to instance scripts
-pikaur -Syu --needed lsd mc reflector expac tmux
-
-# instance project installing
-cd
+# Instance project set installing:
+# IMPORTANT: See notes in .bash_aliases for installing needed packages
+cd ~
 ln -sf /home/$(whoami)/instance/bashrc .bashrc
 ln -s /home/$(whoami)/instance/bash_aliases .bash_aliases
-sudo ln -s /home/$(whoami)/instance/etc/tmux.conf /etc/tmux.conf
-sudo cp /home/$(whoami)/instance/etc/99-sysctl.conf /etc/sysctl.d/99-sysctl.conf
-sudo sysctl --system
 
-# install update timer (ln is only one drive for / and /home!! if not, use cp instead)
-cd instance/update
-sudo ln -s /home/$(whoami)/instance/update/updpkgs.sh /usr/bin/updpkgs
-sudo ln -s /home/$(whoami)/instance/update/update.service /lib/systemd/system/update.service
-sudo ln -s /home/$(whoami)/instance/update/update.timer /lib/systemd/system/update.timer
-sudo systemctl daemon-reload
-sudo systemctl enable --now update.timer
+# Tuning system
+sudo cp /home/$(whoami)/instance/etc/99-sysctl.conf /etc/sysctl.d/99-sysctl.conf && sysctl --system
+
+# 1. Install update timer
+# See instructions in update/install_updatetimer.sh
+
+# 2. Install btrfs snapshot hooks for pacman
+# See instructions in snapshots/install_snapshots.sh
+
+# 3. Install wireguard and wgstat services 
+# See instructions in wireguard/install_wireguard.sh
