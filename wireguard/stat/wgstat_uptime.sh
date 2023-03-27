fb746ead211b9d0d5e6548e7cb6e76103c883b02
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-03-27 13:45:30 +0300

    Renamed wgstat_analyzer.sh

--- wireguard/stat/wgstat_uptime.sh
+++ wireguard/stat/wgstat_uptime.sh
@@ -5,5 +5,5 @@ eth=$(vnstat -i ens3 -m 1 --oneline | cut -d";" -f 11 | cut -d" " -f1)
 
 echo '```'
 printf "       Client     wg       eth    Gbs per day\n"
-/home/$(whoami)/instance/wireguard/stat/wganalyzer.sh | sort -rnk 5 | awk '{printf "%13s %8.2f %8.2f %8.2f\n", $2, $5/1000/1000/1000, $5*'$eth'/'$wg'/1000/1000/1000, $5/'$(date '+%d')'/1000/1000/1000}' | sed 's/0.00     0.00     0.00//g'
+/home/$(whoami)/instance/wireguard/stat/wgstat_analyzer.sh | sort -rnk 5 | awk '{printf "%13s %8.2f %8.2f %8.2f\n", $2, $5/1000/1000/1000, $5*'$eth'/'$wg'/1000/1000/1000, $5/'$(date '+%d')'/1000/1000/1000}' | sed 's/0.00     0.00     0.00//g'
 echo '```'
