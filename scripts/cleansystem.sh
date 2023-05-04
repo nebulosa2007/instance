b60b526f3ae2867b3e0ba1400fd63d30cbb8e6b7
Nebulosa <nebulosa2007@yandex.ru>
2023-05-04 18:40:45 +0300

    Added scrub cleaning operation for btrfs

--- scripts/cleansystem.sh
+++ scripts/cleansystem.sh
@@ -12,4 +12,6 @@ sudo journalctl --disk-usage
 sudo find /var/log -type f -regex ".*\.gz$" -delete 2> /dev/null
 sudo find /var/log -type f -regex ".*\.[0-9]$" -delete 2> /dev/null
 
+[ -f /usr/bin/btrfs ] && (sudo btrfs scrub start /; echo "Waiting 30 seconds..."; sleep 30; sudo btrfs scrub status /)
+
 df -h | grep -E "[s|v]da"
