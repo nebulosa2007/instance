e8eb92f8a6c0d6b03a68bc54453bd59e7e4322ae
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-07-29 18:40:11 +0300

    Fix tabs

--- wireguard/stat/wgstat_uptime.sh
+++ wireguard/stat/wgstat_uptime.sh
@@ -4,6 +4,6 @@ eth=$(vnstat -i ens3 -m 1 --oneline | cut -d";" -f 11 | cut -d" " -f1)
  wg=$(vnstat -i wg0  -m 1 --oneline | cut -d";" -f 11 | cut -d" " -f1)
 
 echo '```'
-printf "       Client     wg       eth    Gbs per day\n"
-/home/$(whoami)/instance/wireguard/stat/wgstat_analyzer.sh | sort -rnk 5 | awk '{printf "%13s %8.2f %8.2f %8.2f\n", $2, $5/1000/1000/1000, $5*'$eth'/'$wg'/1000/1000/1000, $5/'$(date '+%d')'/1000/1000/1000}' | sed 's/0.00     0.00     0.00//g'
+printf "        Client     wg       eth    Gbs per day\n"
+/home/$(whoami)/instance/wireguard/stat/wgstat_analyzer.sh | sort -rnk 5 | awk '{printf "%14s %8.2f %8.2f %8.2f\n", $2, $5/1000/1000/1000, $5*'$eth'/'$wg'/1000/1000/1000, $5/'$(date '+%d')'/1000/1000/1000}' | sed 's/0.00     0.00     0.00//g'
 echo '```'
