248bbaaf8b5a7b8b3850c7b02cabc8d03723c1eb
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-08-08 08:49:54 +0300

    Fixed curl request

--- scripts/tgsay.sh
+++ scripts/tgsay.sh
@@ -11,10 +11,10 @@ if [ -z "$CHAT_ID" ]; then
     exit 1
 fi
 
-#MSG="$@"
+MSG=$(echo "$@" | tr '\n' '[' | sed 's/ /%20/g;s/\[/%0A/g')
 
 #/usr/bin/wget -qO- "https://api.telegram.org/bot$API_TOKEN/sendMessage?chat_id=$CHAT_ID&parse_mode=html&text=$@" 2>&1
-/usr/bin/curl "https://api.telegram.org/bot$API_TOKEN/sendMessage?chat_id=$CHAT_ID&parse_mode=html&text=$@" 2>&1
+/usr/bin/curl "https://api.telegram.org/bot$API_TOKEN/sendMessage?chat_id=$CHAT_ID&parse_mode=HTML&text=$MSG" 2>&1
 
 #echo "https://api.telegram.org/bot$API_TOKEN/sendMessage?chat_id=$CHAT_ID&parse_mode=html&text=$@"
 #if [ $? -eq 0 ]; then
