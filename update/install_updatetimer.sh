9f75924a265a704a783235375f656c1ed5f437b4
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-08-30 18:03:01 +0300

    Fix way for timers

--- update/install_updatetimer.sh
+++ update/install_updatetimer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo cp /home/$(whoami)/instance/update/updpkgs.sh /usr/bin/updpkgs
+sudo cp /home/$(whoami)/instance/update/updpkgs.sh     /usr/bin/updpkgs
 sudo cp /home/$(whoami)/instance/update/update.service /lib/systemd/system/update.service
-sudo cp /home/$(whoami)/instance/update/update.timer /lib/systemd/system/update.timer
+sudo cp /home/$(whoami)/instance/update/update.timer   /etc/systemd/system/update.timer
 sudo systemctl daemon-reload
 sudo systemctl enable --now update.timer
