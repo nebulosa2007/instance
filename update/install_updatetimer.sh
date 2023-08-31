854629ddd5bfaf92936e733de559a2f8f2a3609e
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-08-31 10:39:27 +0300

    Check if wait online servies is working

--- update/install_updatetimer.sh
+++ update/install_updatetimer.sh
@@ -4,4 +4,8 @@ sudo cp /home/$(whoami)/instance/update/updpkgs.sh     /usr/bin/updpkgs
 sudo cp /home/$(whoami)/instance/update/update.service /lib/systemd/system/update.service
 sudo cp /home/$(whoami)/instance/update/update.timer   /lib/systemd/system/update.timer
 sudo systemctl daemon-reload
+
+#Check if any "wait-online-service" is working. Credits: https://wiki.archlinux.org/title/Systemd-networkd#systemd-networkd-wait-online
+systemctl is-enabled NetworkManager-wait-online.service systemd-networkd-wait-online.service
+
 sudo systemctl enable --now update.timer
