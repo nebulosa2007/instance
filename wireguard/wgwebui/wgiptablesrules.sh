97dcaca016406c5695aa0288a63cd0fd55032dd7
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-10-13 23:39:41 +0300

    Refactoring, using wg server params

--- wireguard/wgwebui/wgiptablesrules.sh
+++ wireguard/wgwebui/wgiptablesrules.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-#SERVER_PORT=$(ss -ul | grep -Eo "[0-9]{4,5}")
-SERVER_PUB_NIC="$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)"
+source /etc/wireguard/params 
 
 if [ "$1" == "up" ]
 then
-  #iptables -I INPUT -p udp --dport $SERVER_PORT -j ACCEPT 
-  iptables -I FORWARD -i $SERVER_PUB_NIC -o %i -j ACCEPT
-  iptables -I FORWARD -i %i -j ACCEPT
-  iptables -t nat -A POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE
-  ip6tables -I FORWARD -i %i -j ACCEPT
-  ip6tables -t nat -A POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE
+  iptables -I INPUT -p udp --dport ${SERVER_PORT} -j ACCEPT
+  iptables -I FORWARD -i ${SERVER_PUB_NIC} -o ${SERVER_WG_NIC} -j ACCEPT
+  iptables -I FORWARD -i ${SERVER_WG_NIC} -j ACCEPT
+  iptables -t nat -A POSTROUTING -o ${SERVER_PUB_NIC} -j MASQUERADE
+  ip6tables -I FORWARD -i ${SERVER_WG_NIC} -j ACCEPT
+  ip6tables -t nat -A POSTROUTING -o ${SERVER_PUB_NIC} -j MASQUERADE
 else
   if [ "$1" == "down" ]
   then
-    #iptables -D INPUT -p udp --dport $SERVER_PORT -j ACCEPT
-    iptables -D FORWARD -i $SERVER_PUB_NIC -o %i -j ACCEPT
-    iptables -D FORWARD -i %i -j ACCEPT
-    iptables -t nat -D POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE
-    ip6tables -D FORWARD -i %i -j ACCEPT
-    ip6tables -t nat -D POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE
-  fi
+    iptables -D INPUT -p udp --dport ${SERVER_PORT} -j ACCEPT
+    iptables -D FORWARD -i ${SERVER_PUB_NIC} -o ${SERVER_WG_NIC} -j ACCEPT
+    iptables -D FORWARD -i ${SERVER_WG_NIC} -j ACCEPT
+    iptables -t nat -D POSTROUTING -o ${SERVER_PUB_NIC} -j MASQUERADE
+    ip6tables -D FORWARD -i ${SERVER_WG_NIC} -j ACCEPT
+    ip6tables -t nat -D POSTROUTING -o ${SERVER_PUB_NIC} -j MASQUERADE
+   fi
 fi
