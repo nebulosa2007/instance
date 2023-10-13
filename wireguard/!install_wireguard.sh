a3b8e6561a8f7a85f6e8dd72c9816d4767dd0ec1
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-10-13 17:06:25 +0300

    Wireguard UI init release

--- wireguard/!install_wireguard.sh
+++ wireguard/!install_wireguard.sh
@@ -10,18 +10,38 @@ mkdir -p var && cd var
 curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
 chmod +x wireguard-install.sh
 sudo ./wireguard-install.sh
+# you need install server only!
+sudo systemctl stop wg-quick@wg0
 
-# Please save all *.conf files in instance/wireguard/var directory
-
-# For wgstat for TG script without sudo
-echo "%wheel ALL=(ALL:ALL) NOPASSWD:/usr/bin/wg show wg0" | sudo tee /etc/sudoers.d/wgstatus
-sudo chmod 440 /etc/sudoers.d/wgstatus
-sudo visudo -c
-
-# Install wgstat service
-#IMPORTANT: Only for one partition systems. Use cp instead ln below:
-sudo ln -sf $PATHINSTANCE/wireguard/stat/wgstat     /usr/bin/wgstat
-sudo cp $PATHINSTANCE/wireguard/stat/wgstat.service /lib/systemd/system/wgstat.service
-sudo cp $PATHINSTANCE/wireguard/stat/wgstat.timer   /lib/systemd/system/wgstat.timer
+# Credits: https://github.com/ngoduykhanh/wireguard-ui
+sudo curl -sL $(curl -s https://api.github.com/repos/ngoduykhanh/wireguard-ui/releases/latest | grep -Eom1 "https://.*linux-amd64.tar.gz") | sudo tar xz --one-top-level=/opt/wireguard-ui/
+sudo cp $PATHINSTANCE/wireguard/wgwebui/wgui-reload-conf.{service,path} -t /lib/systemd/system/
+sudo cp $PATHINSTANCE/wireguard/wgwebui/wgui.service /lib/systemd/system/wgui.service
+sudo cp $PATHINSTANCE/wireguard/wgwebui/wgiptablesrules.sh /opt/wireguard-ui/wgiptablesrules
 sudo systemctl daemon-reload
-sudo systemctl enable --now wgstat.timer
+sudo systemctl enable --now wgui-reload-conf.{service,path} {wgui,wg-quick@wg0}
+# Then open in browser ip_server:5000
+# Login and password is admin:admin. Login and change password for you own!!
+# Specify in server settings PostUp and PostDown script:
+# PostUp:   "/opt/wireguard-ui/wgiptablesrules up"
+# PostDown: "/opt/wireguard-ui/wgiptablesrules down"
+# Save server settings and check connection for some user 
+# Status wgui-reload-conf.{service,path} {wgui,wg-quick@wg0}
+
+
+## Under construction:
+# mkdir -p var && cd var
+# # Please save all *.conf files in instance/wireguard/var directory
+#
+# # For wgstat for TG script without sudo
+# echo "%wheel ALL=(ALL:ALL) NOPASSWD:/usr/bin/wg show wg0" | sudo tee /etc/sudoers.d/wgstatus
+# sudo chmod 440 /etc/sudoers.d/wgstatus
+# sudo visudo -c
+#
+# # Install wgstat service
+# #IMPORTANT: Only for one partition systems. Use cp instead ln below:
+# sudo ln -sf $PATHINSTANCE/wireguard/stat/wgstat     /usr/bin/wgstat
+# sudo cp $PATHINSTANCE/wireguard/stat/wgstat.service /lib/systemd/system/wgstat.service
+# sudo cp $PATHINSTANCE/wireguard/stat/wgstat.timer   /lib/systemd/system/wgstat.timer
+# sudo systemctl daemon-reload
+# sudo systemctl enable --now wgstat.timer
