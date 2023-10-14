18806ab71132ee79fca3ed57762379c4f63c1da6
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-10-14 18:59:05 +0300

    Clarified ways for all services

--- update/!install_updatetimer.sh
+++ update/!install_updatetimer.sh
@@ -5,8 +5,7 @@ source /etc/instance.conf
 #IMPORTANT: Only for one partition systems. Use cp instead ln below:
 sudo ln -sf $PATHINSTANCE/update/updpkgs.sh /usr/bin/updpkgs
 
-sudo cp $PATHINSTANCE/update/update.service /lib/systemd/system/update.service
-sudo cp $PATHINSTANCE/update/update.timer   /lib/systemd/system/update.timer
+sudo cp $PATHINSTANCE/update/update.{service,timer} -t /etc/systemd/system
 sudo systemctl daemon-reload
 
 #Check if any "wait-online-service" is working. Credits: https://wiki.archlinux.org/title/Systemd-networkd#systemd-networkd-wait-online
--- wireguard/!install_wireguard.sh
+++ wireguard/!install_wireguard.sh
@@ -1,35 +1,35 @@
 #Wireguard module of Instance project
 
-# Lasy install wireguard server. WARNING: this setup wouldn't work with wg-gui etc.
-
 source /etc/instance.conf
 cd $PATHINSTANCE/wireguard
 mkdir -p var && cd var
 
-# Credits: https://github.com/angristan/wireguard-install
-curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
-chmod +x wireguard-install.sh
-sudo ./wireguard-install.sh
-# you need install server only!
-sudo systemctl stop wg-quick@wg0
+# https://github.com/angristan/wireguard-install
+# curl -O https://raw.githubusercontent.com/angristan/wireguard-install/master/wireguard-install.sh
+# chmod +x wireguard-install.sh
+# sudo ./wireguard-install.sh
+# # you need install server only!
+# sudo systemctl stop wg-quick@wg0
 
-# Credits: https://github.com/ngoduykhanh/wireguard-ui
-sudo curl -sL $(curl -s https://api.github.com/repos/ngoduykhanh/wireguard-ui/releases/latest | grep -Eom1 "https://.*linux-amd64.tar.gz") | sudo tar xz --one-top-level=/opt/wireguard-ui/
-sudo cp $PATHINSTANCE/wireguard/wgwebui/wgui-reload-conf.{service,path} -t /lib/systemd/system/
-sudo cp $PATHINSTANCE/wireguard/wgwebui/wgui.service /lib/systemd/system/wgui.service
-sudo cp $PATHINSTANCE/wireguard/wgwebui/wgiptablesrules.sh /opt/wireguard-ui/wgiptablesrules
-sudo systemctl daemon-reload
-sudo systemctl enable --now wgui-reload-conf.{service,path} {wgui,wg-quick@wg0}
+# https://github.com/ngoduykhanh/wireguard-ui
+# Install wireguard-tools
+# sudo curl -sL $(curl -s https://api.github.com/repos/ngoduykhanh/wireguard-ui/releases/latest | grep -Eom1 "https://.*linux-amd64.tar.gz") | sudo tar xz --one-top-level=/opt/wireguard-ui/
+# sudo cp $PATHINSTANCE/wireguard/wgwebui/wg-reload.{service,path} -t /usr/lib/systemd/system
+# sudo cp $PATHINSTANCE/wireguard/wgwebui/wireguard-ui.service -t /usr/lib/systemd/system
+# sudo cp $PATHINSTANCE/wireguard/wgwebui/wgiptables.sh /opt/wireguard-ui/wgiptablesrules
+# sudo systemctl daemon-reload
+# sudo systemctl enable --now wg-reload wireguard-ui wg-quick@wg0
+
 # Then open in browser ip_server:5000
-# Login and password is admin:admin. Login and change password for you own!!
+# The  default username and password are admin. Please change it to secure your setup.
 # Specify in server settings PostUp and PostDown script:
 # PostUp:   "/opt/wireguard-ui/wgiptablesrules up"
 # PostDown: "/opt/wireguard-ui/wgiptablesrules down"
 # Save server settings and check connection for some user 
-# Status wgui-reload-conf.{service,path} {wgui,wg-quick@wg0}
+# systemctl status --no-pager -l wg-reload wireguard-ui wg-quick@wg0
 
 
-## Under construction:
+## Under construction, traffic logger:
 # mkdir -p var && cd var
 # # Please save all *.conf files in instance/wireguard/var directory
 #
@@ -40,8 +40,8 @@ sudo systemctl enable --now wgui-reload-conf.{service,path} {wgui,wg-quick@wg0}
 #
 # # Install wgstat service
 # #IMPORTANT: Only for one partition systems. Use cp instead ln below:
-# sudo ln -sf $PATHINSTANCE/wireguard/stat/wgstat     /usr/bin/wgstat
-# sudo cp $PATHINSTANCE/wireguard/stat/wgstat.service /lib/systemd/system/wgstat.service
-# sudo cp $PATHINSTANCE/wireguard/stat/wgstat.timer   /lib/systemd/system/wgstat.timer
+# sudo ln -sf $PATHINSTANCE/wireguard/stat/wgstat /usr/bin/wgstat
+#
+# sudo cp $PATHINSTANCE/wireguard/stat/wgstat.{service,timer} -t /etc/systemd/system
 # sudo systemctl daemon-reload
 # sudo systemctl enable --now wgstat.timer
