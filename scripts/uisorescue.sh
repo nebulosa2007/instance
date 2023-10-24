dd008af27b5e96d0a434381d25ecad3bb70ae1eb
Nebulosa <nebulosa2007@yandex.ru>
2023-10-25 00:32:27 +0300

    Fixies

--- scripts/uisorescue.sh
+++ scripts/uisorescue.sh
@@ -33,19 +33,24 @@ then
     # make subvolume if it isn't exist and update grub entry:
     if [ "$(sudo btrfs subvolume list / | awk '/level 5/ && /'$SUBVOL'/ {print $NF}'| head -n1)" == "" ]
     then
-        sudo mount $ROOTDRIVE /mnt && (cd /mnt && sudo btrfs subvolume create $SUBVOL && cd / && sudo umount /mnt) || exit 1
+        sudo mount $ROOTDRIVE /mnt && cd /mnt && sudo btrfs subvolume create $SUBVOL && cd / && sudo umount /mnt
         if [ $? -eq 0 ]
         then
             sudo sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=1/' /etc/default/grub
             cat $PATHINSTANCE/etc/40_custom.menuentry | sudo tee -a /etc/grub.d/40_custom > /dev/null && sudo grub-mkconfig -o /boot/grub/grub.cfg
+        else
+            echo "Error of creating $SUBVOL"
+            exit 1
         fi
     fi
-    sudo mkdir -p $FOLDER && sudo mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=$SUBVOL $ROOTDRIVE $FOLDER || exit 1
+    sudo mkdir -p $FOLDER && sudo mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=$SUBVOL $ROOTDRIVE $FOLDER
     if [ $? -eq 0 ]
     then
         sudo curl -o "$FOLDER/$ISO" "FILE:///home/$(whoami)/$ISO"
         cd $FOLDER && curl -s $MIRROR"sha256sums.txt" | grep $ISO | sha256sum -c --
         cd / && sudo umount $FOLDER && sudo rm -r $FOLDER
         rm /home/$(whoami)/$ISO
+    else
+        echo "Error of mounting $SUBVOL"
     fi
 fi
