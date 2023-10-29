0b69217ba6c5b8aeacbe4700333e1adcde230157
Nebulosa <nebulosa2007@yandex.ru>
2023-10-29 12:51:30 +0300

    Yabsnap refactoring

--- snapshots/snaplist.sh
+++ snapshots/snaplist.sh
@@ -7,14 +7,22 @@ nc='\033[0m'
 
 function print_snapshot_log
 {
-    cat /var/log/pacman.log | while read pacman_log_line
+	hookfile="01-yabsnap-pacman-pre.hook"
+    cat /var/log/pacman.log | grep "running '"$hookfile"'" | while read pacman_log_line
     do
+        
         if [[ $pacman_log_line == *"[$snapshot]"* ]];
         then
-            filtered_pacman_log_line=$(echo $pacman_log_line | cut -d" " -f4- | sed "s/Running //;s/--color=always //;s/--needed //;s/'//g;s/\/usr\/bin\///;s/pacman //")
-            operation=$(echo $filtered_pacman_log_line | cut -d" " -f1)
-            packages=$(echo $(awk '/'$snapshot'/,/transaction completed$/' /var/log/pacman.log | grep -E "\[ALPM\] (removed|installed|downgraded|upgraded|reinstalled)" | cut -d" " -f4-7 | sed 's/)/,/;s/(//') | sed 's/,$/ /')
-
+            operation=$(echo $pacman_log_line | cut -d" " -f4- | \
+            sed "s/Running //;\
+                 s/--color=always //;\
+                 s/--needed //;s/'//g;\
+                 s/\/usr\/bin\///;\
+                 s/pacman //" | \
+            cut -d" " -f1)
+            packages=$(echo $(awk '/'$snapshot'/,/transaction completed$/' /var/log/pacman.log | \
+                       grep -E "\[ALPM\] (removed|installed|downgraded|upgraded|reinstalled)" | \
+                       cut -d" " -f4-7 | sed 's/)/,/;s/(//') | sed 's/,$/ /')
             case $operation in
             "-S"  | "--sync"   ) color=$green;  action="install";;
             "-R"* | "--remove" ) color=$red;    action="delet";;
@@ -29,18 +37,17 @@ function print_snapshot_log
 
 if [ -z $1 ]
 then
-    #/ snapshots
-    printf "List of root snapshots:${green} $(sudo btrfs subvolume list / | awk '/root-snapshot/' | wc -l) ${nc}\n\n"
-    sudo btrfs subvolume list / | awk '/root-snapshot/ {print $NF}'| while read snapshot
+    SNAPDIR="/.snapshots"
+    LISTROOT=$(ls -d -1 $SNAPDIR/*/ 2>/dev/null | tr " " "\n" | sed 's/\/$//g')
+    printf "List of snapshots:${green} $(echo $LISTROOT | wc -w) ${nc}\n\n"
+    echo $LISTROOT | tr " " "\n" | while read snapshot
     do
-        print_snapshot_log $snapshot
+        echo -n $(basename $snapshot)": "
+        cat $snapshot-meta.json | echo $(tr -d '"{}')
     done
-    printf "\n"
-    #/home snapshots
-    printf "List of home snapshots:${green} $(sudo btrfs subvolume list / | awk '/home-snapshot/'| wc -l) ${nc}\n\n"
-    sudo btrfs subvolume list / | awk '/home-snapshot/ {print $1"="$2" "$NF}'
 else
-    snapshot=$1
-    LOG=$(print_snapshot_log $snapshot)
-    [ "$LOG" == "" ] && echo "No comments about this shapshot found" || echo $LOG
+    #snapshot=$1
+    cat $1-meta.json | echo $(tr -d '"{}')
+    #LOG=$(print_snapshot_log $snapshot)
+    #[ "$LOG" == "" ] && echo "No comments about this shapshot found" || echo $LOG
 fi
