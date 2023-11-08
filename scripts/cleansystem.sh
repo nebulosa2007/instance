dd869f9daf6470d543cb073149c36fe66c968850
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-11-08 13:11:32 +0300

    Added wiki archlinux links

--- scripts/cleansystem.sh
+++ scripts/cleansystem.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
 df -h | grep -E "$( [ "$(mount | grep -Po '(?<= on \/ type )(\S+)')" == "btrfs" ] && echo '/$' || echo '/[s|v]da' )"
+
+# https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#Removing_unused_packages_(orphans)
 sudo pacman -Rsn $(pacman -Qdtq)
+
+# https://wiki.archlinux.org/title/Pacman#Package_cache_directory
+# In case Cache folder is '/tmp' to do not flush other files
 sudo rm -f $(awk '/Cache/ {print $3}' /etc/pacman.conf)/*.pkg.tar.{zst,zst.sig}
 
+# https://wiki.archlinux.org/title/Systemd/Journal#Clean_journal_files_manually
 sudo journalctl --disk-usage
 sudo journalctl --vacuum-size=5M
 sudo journalctl --verify
 sudo journalctl --disk-usage
 
+# Remove old log files
 sudo find /var/log -type f -regex ".*\.gz$" -delete 2> /dev/null
 sudo find /var/log -type f -regex ".*\.[0-9]$" -delete 2> /dev/null
 
-#CLEANING PIKAUR CACHE
+# Cleaning pikaur cache
 find /home/$(whoami)/.cache/pikaur/build -delete 2> /dev/null
 find /home/$(whoami)/.cache/pikaur/pkg -delete 2> /dev/null
 
+# https://wiki.archlinux.org/title/Btrfs#Scrub
 if [ -x "$(command -v btrfs)" ]; then
   sudo btrfs scrub start /
   for i in . . . . .; do echo -n $i; sleep 1; done
   while [ "$(sudo btrfs scrub status / | grep 'running')" != "" ] ; do echo -n "." ; sleep 1; done; echo
   sudo btrfs scrub status /
 fi
+
+#todo https://wiki.archlinux.org/title/Btrfs#Balance
 
 df -h | grep -E "$( [ "$(mount | grep -Po '(?<= on \/ type )(\S+)')" == "btrfs" ] && echo '/$' || echo '/[s|v]da' )"
