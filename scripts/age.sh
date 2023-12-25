f8d622d9fd82c24d9087ab871c56b0ac175037be
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-12-25 13:03:46 +0300

    Refactor for posting

--- scripts/age.sh
+++ scripts/age.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#METHOD=$(stat /etc | tail -1 | cut -d" " -f3)
-METHOD=$(/usr/bin/ls -ctl --time-style +"%Y-%m-%d" /etc | tail -1 | grep -Po "[0-9]+-[0-9]+-[0-9]+")
-#METHOD=$(head -1 /var/log/pacman.log | cut -c 2-11)
+DATE=$(/usr/bin/ls -ctl --time-style +"%Y-%m-%d" /etc | tail -1 | grep -Po "[0-9]+-[0-9]+-[0-9]+")
+#or
+#DATE=$(stat /etc | tail -1 | cut -d" " -f3)
+#or
+#DATE=$(head -1 /var/log/pacman.log | cut -c 2-11)
 
-DATE_LOCAL=$(echo $METHOD | sed 's/-/ /g' | awk '{print $3"."$2"."$1}')
-DAYSBETWEEN=$(( ($(date -d $(date +%Y-%m-%d) +%s) - $(date -d $METHOD +%s)) / 86400 ))
+LOCALE=$(echo $DATE | sed 's/-/ /g' | awk '{print $3"."$2"."$1}')
+DAYSBETWEEN=$(( ($(date -d $(date +%Y-%m-%d) +%s) - $(date -d $DATE +%s)) / 86400 ))
 
 YEARSCALC=$(( $DAYSBETWEEN / 365 ))
 MONTHSCALC=$(( ($DAYSBETWEEN - ($YEARSCALC * 365)) / 30 ))
@@ -15,4 +17,4 @@ DAYSCALC=$(( $DAYSBETWEEN -($YEARSCALC * 365) - ($MONTHSCALC * 30) ))
 [[ $MONTHSCALC > 0 ]] && MONTHS=$MONTHSCALC"m "
 [[ $DAYSCALC > 0   ]] && DAYS=$DAYSCALC"d"
 
-echo "System age: "$YEARS$MONTHS$DAYS" (since $DATE_LOCAL)"
+echo "System age: "$YEARS$MONTHS$DAYS" (since $LOCALE)"
