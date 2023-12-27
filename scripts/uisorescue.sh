217dce0055cf1c486e062a6863183b3320a91f5d
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-12-27 18:58:23 +0300

    Refact

--- scripts/uisorescue.sh
+++ scripts/uisorescue.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-source /etc/instance.conf
+MIRROR="mirror.ams1.nl.leaseweb.net"
 SUBVOL="@archiso"
 ISO="archlinux-x86_64.iso"
-MIRROR="mirror.ams1.nl.leaseweb.net"
 FOLDER="/mnt/archiso"
 
 ROOTDRIVE=$(mount | grep -Po '^.*(?= on \/ type btrfs)')
@@ -14,37 +13,47 @@ ROOTDRIVE=$(mount | grep -Po '^.*(?= on \/ type btrfs)')
 
 function writetogrub(){
     sudo sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=1/' /etc/default/grub
-    if [ $(cat /etc/grub.d/40_custom | wc -l) -eq 5 ]
-    then
-       cat $PATHINSTANCE/etc/40_custom.menuentry | sudo tee -a /etc/grub.d/40_custom > /dev/null && sudo grub-mkconfig -o /boot/grub/grub.cfg
+    menuentry="menuentry 'Boot from archlinux.iso' {
+    load_video
+    set gfxpayload=keep
+    insmod gzio
+    insmod part_gpt
+    insmod btrfs
+    insmod loopback
+    probe -u $root --set=rootuuid
+    set imgdevpath=\"/dev/disk/by-uuid/$rootuuid\"
+    set isofile='/@archiso/archlinux-x86_64.iso'
+    loopback loop $isofile
+    linux (loop)/arch/boot/x86_64/vmlinuz-linux img_dev=$imgdevpath img_loop=$isofile earlymodules=loop
+    initrd (loop)/arch/boot/x86_64/initramfs-linux.img
+}"
+    if [ $(cat /etc/grub.d/40_custom | wc -l) -eq 5 ]; then
+       echo menuentry | sudo tee -a /etc/grub.d/40_custom > /dev/null && sudo grub-mkconfig -o /boot/grub/grub.cfg
     fi
 }
 
 
 function checkiso(){
-	curl -s "https://"$MIRROR"/archlinux/iso/latest/sha256sums.txt" | grep $ISO | sha256sum -c --
+	curl -s "https://$MIRROR/archlinux/iso/latest/sha256sums.txt" | grep $ISO | sha256sum -c --
 }
 
 
-if [ "$(sudo btrfs subvolume list / | grep 'top level [0-9] path '$SUBVOL)" == "" ]
-then
+if [ "$(sudo btrfs subvolume list / | grep 'top level [0-9] path '$SUBVOL)" == "" ]; then
 	sudo mount $ROOTDRIVE /mnt
 	cd /mnt && sudo btrfs subvolume create $SUBVOL && cd /
 	sudo umount /mnt && writetogrub || echo "Error of creating $SUBVOL!"
 fi
 
 
-if [ "$(sudo btrfs subvolume list / | grep 'top level [0-9] path '$SUBVOL)" != "" ]
-then
+if [ "$(sudo btrfs subvolume list / | grep 'top level [0-9] path '$SUBVOL)" != "" ]; then
     sudo mkdir -p $FOLDER && sudo mount -o compress=zstd:3,subvol=$SUBVOL $ROOTDRIVE $FOLDER
     cd $FOLDER
-    if checkiso
-    then
+    if checkiso; then
         echo "The system already has latest iso image of Archlinux. Nothing to do"
     else
         echo "Downloading $ISO from $MIRROR ..."
         cd /home/$(whoami)
-        curl -L -O -C - "https://"$MIRROR"/archlinux/iso/latest/$ISO"
+        curl -L -O -C - "https://$MIRROR/archlinux/iso/latest/$ISO"
         sudo curl -o "$FOLDER/$ISO" "FILE:///home/$(whoami)/$ISO"
         checkiso && rm /home/$(whoami)/$ISO || echo "Checksum error!"
     fi
