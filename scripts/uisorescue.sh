8123d440ca2ed93d760dc2db2d4c84fa78e926ce
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-12-27 13:50:11 +0300

    Refact

--- scripts/uisorescue.sh
+++ scripts/uisorescue.sh
@@ -15,7 +15,10 @@ ROOTDRIVE=$(mount | grep -Po '^.*(?= on \/ type btrfs)')
 
 function writetogrub(){
     sudo sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=1/' /etc/default/grub
-    cat $PATHINSTANCE/etc/40_custom.menuentry | sudo tee -a /etc/grub.d/40_custom > /dev/null && sudo grub-mkconfig -o /boot/grub/grub.cfg
+    if [ $(cat /etc/grub.d/40_custom | wc -l) -eq 5 ]
+    then
+       cat $PATHINSTANCE/etc/40_custom.menuentry | sudo tee -a /etc/grub.d/40_custom > /dev/null && sudo grub-mkconfig -o /boot/grub/grub.cfg
+    fi
 }
 
 
@@ -23,14 +26,23 @@ function mountarchiso(){
 	sudo mkdir -p $FOLDER && sudo mount -o compress=zstd:3,subvol=$SUBVOL $ROOTDRIVE $FOLDER
 }
 
+function umountarchiso(){
+	cd / && sudo umount $FOLDER && sudo rm -r $FOLDER
+}
+
+
+function checkiso(){
+	cd $FOLDER && curl -s $MIRROR"sha256sums.txt" | grep $ISO | sha256sum -c --
+}
+
 
 #If it already done on btrfs disk?
-if [ "$(sudo btrfs subvolume list / | awk '/level 5/ && /'$SUBVOL'/ {print $NF}'| head -n1)" != "" ]
+if [ "$(sudo btrfs subvolume list / | grep 'top level [0-9] path '$SUBVOL)" != "" ]
 then
+    writetogrub
     mountarchiso
-    cd $FOLDER && curl -s $MIRROR"sha256sums.txt" | grep $ISO | sha256sum -c -- && echo "The system already has latest iso image of Archlinux. Nothing to do" && ALLDONE=1
-    cd / && sudo umount $FOLDER && sudo rm -r $FOLDER
-    [ $(cat /etc/grub.d/40_custom | wc -l) -eq 5 ] && writetogrub
+    checkiso && echo "The system already has latest iso image of Archlinux. Nothing to do" && ALLDONE=1
+    umountarchiso
     [ $ALLDONE -eq 1 ] && exit 1
 fi
 
@@ -42,17 +54,15 @@ curl -s $MIRROR"sha256sums.txt" | grep $ISO | sha256sum -c --
 if [ $? -eq 0 ]
 then
     # make subvolume if it isn't exist and update grub entry:
-    if [ "$(mount | grep -Po '(?<= on \/ type )(\S+)')" == "btrfs" ]
-    then
-        sudo mount $ROOTDRIVE /mnt && cd /mnt && sudo btrfs subvolume create $SUBVOL && cd / && sudo umount /mnt
-        [ $? -eq 0 ] && writetogrub || { echo "Error of creating $SUBVOL"; exit 1; }
-    fi
+    sudo mount $ROOTDRIVE /mnt && cd /mnt && sudo btrfs subvolume create $SUBVOL && cd / && sudo umount /mnt
+    [ $? -eq 0 ] || { echo "Error of creating $SUBVOL"; exit 1; }
+    writetogrub
     mountarchiso
     if [ $? -eq 0 ]
     then
         sudo curl -o "$FOLDER/$ISO" "FILE:///home/$(whoami)/$ISO"
-        cd $FOLDER && curl -s $MIRROR"sha256sums.txt" | grep $ISO | sha256sum -c --
-        cd / && sudo umount $FOLDER && sudo rm -r $FOLDER
+        checkiso || echo "Checksum error!"
+        umountarchiso
         rm /home/$(whoami)/$ISO
     else
         echo "Error of mounting $SUBVOL"
