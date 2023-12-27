a52669050d3fe99e89e6d2d580659588a9a16327
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-12-27 19:45:42 +0300

    fix menuentry

--- scripts/uisorescue.sh
+++ scripts/uisorescue.sh
@@ -13,7 +13,8 @@ ROOTDRIVE=$(mount | grep -Po '^.*(?= on \/ type btrfs)')
 
 function writetogrub(){
     sudo sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=1/' /etc/default/grub
-    menuentry="menuentry 'Boot from archlinux.iso' {
+    cat << EOF > /tmp/40_custom
+menuentry 'Boot from archlinux.iso' {
     load_video
     set gfxpayload=keep
     insmod gzio
@@ -21,14 +22,16 @@ function writetogrub(){
     insmod btrfs
     insmod loopback
     probe -u $root --set=rootuuid
-    set imgdevpath=\"/dev/disk/by-uuid/$rootuuid\"
+    set imgdevpath='/dev/disk/by-uuid/$rootuuid'
     set isofile='/@archiso/archlinux-x86_64.iso'
     loopback loop $isofile
     linux (loop)/arch/boot/x86_64/vmlinuz-linux img_dev=$imgdevpath img_loop=$isofile earlymodules=loop
     initrd (loop)/arch/boot/x86_64/initramfs-linux.img
-}"
+}
+EOF
     if [ $(cat /etc/grub.d/40_custom | wc -l) -eq 5 ]; then
-       echo menuentry | sudo tee -a /etc/grub.d/40_custom > /dev/null && sudo grub-mkconfig -o /boot/grub/grub.cfg
+       cat /tmp/40_custom | sudo tee -a /etc/grub.d/40_custom > /dev/null && sudo grub-mkconfig -o /boot/grub/grub.cfg
+       rm /tmp/40_custom
     fi
 }
 
