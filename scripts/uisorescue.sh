efa0a99996796d58c709648b85b0f9b12a604022
Nebulosa2007 <nebulosa2007@yandex.ru>
2023-12-27 11:58:00 +0300

    Add mountarchiso

--- scripts/uisorescue.sh
+++ scripts/uisorescue.sh
@@ -12,15 +12,22 @@ ROOTDRIVE=$(mount | grep -Po '^.*(?= on \/ type btrfs)')
 [ "$ROOTDRIVE" == "" ] && echo "This script works only with BTRFS" && exit 1
 [ ! -e /etc/default/grub ] && echo "This script works only with GRUB" && exit 1
 
+
 function writetogrub(){
     sudo sed -i 's/GRUB_TIMEOUT=0/GRUB_TIMEOUT=1/' /etc/default/grub
     cat $PATHINSTANCE/etc/40_custom.menuentry | sudo tee -a /etc/grub.d/40_custom > /dev/null && sudo grub-mkconfig -o /boot/grub/grub.cfg
 }
 
+
+function mountarchiso(){
+	sudo mkdir -p $FOLDER && sudo mount -o compress=zstd:3,subvol=$SUBVOL $ROOTDRIVE $FOLDER
+}
+
+
 #If it already done on btrfs disk?
 if [ "$(sudo btrfs subvolume list / | awk '/level 5/ && /'$SUBVOL'/ {print $NF}'| head -n1)" != "" ]
 then
-    sudo mkdir -p $FOLDER && sudo mount -o compress=zstd:3,subvol=$SUBVOL $ROOTDRIVE $FOLDER
+    mountarchiso
     cd $FOLDER && curl -s $MIRROR"sha256sums.txt" | grep $ISO | sha256sum -c -- && echo "The system already has latest iso image of Archlinux. Nothing to do" && ALLDONE=1
     cd / && sudo umount $FOLDER && sudo rm -r $FOLDER
     [ $(cat /etc/grub.d/40_custom | wc -l) -eq 5 ] && writetogrub
@@ -40,7 +47,7 @@ then
         sudo mount $ROOTDRIVE /mnt && cd /mnt && sudo btrfs subvolume create $SUBVOL && cd / && sudo umount /mnt
         [ $? -eq 0 ] && writetogrub || { echo "Error of creating $SUBVOL"; exit 1; }
     fi
-    sudo mkdir -p $FOLDER && sudo mount -o compress=zstd:3,subvol=$SUBVOL $ROOTDRIVE $FOLDER
+    mountarchiso
     if [ $? -eq 0 ]
     then
         sudo curl -o "$FOLDER/$ISO" "FILE:///home/$(whoami)/$ISO"
