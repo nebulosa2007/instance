9059c84b56a7fe5498b127eae592c023e7497772
Nebulosa <nebulosa@yandex.ru>
2024-02-16 12:53:13 +0300

    Shellcheck respect

--- scripts/cleansystem.sh
+++ scripts/cleansystem.sh
@@ -3,11 +3,11 @@
 df -h | grep -E "$( [ "$(mount | grep -Po '(?<= on \/ type )(\S+)')" == "btrfs" ] && echo '/$' || echo '/[s|v]da' )"
 
 # https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#Removing_unused_packages_(orphans)
-sudo pacman -Rsn $(pacman -Qdtq)
+sudo pacman -Rsn "$(pacman -Qdtq)"
 
 # https://wiki.archlinux.org/title/Pacman#Package_cache_directory
 # In case Cache folder is '/tmp' to do not wipe other files
-sudo find $(grep -Po '(?<=CacheDir) *= *\K(\S+)' /etc/pacman.conf) -type f -name "*.pkg.tar.zst*" -delete 2> /dev/null
+sudo find "$(grep -Po '(?<=CacheDir) *= *\K(\S+)' /etc/pacman.conf)" -type f -name "*.pkg.tar.zst*" -delete 2> /dev/null
 
 # https://wiki.archlinux.org/title/Systemd/Journal#Clean_journal_files_manually
 sudo journalctl --disk-usage
@@ -20,8 +20,8 @@ sudo find /var/log -type f -regex ".*\.gz$" -delete 2> /dev/null
 sudo find /var/log -type f -regex ".*\.[0-9]$" -delete 2> /dev/null
 
 # Cleaning pikaur cache
-[ -d "/home/$(whoami)/.cache/pikaur/build" ] && find /home/$(whoami)/.cache/pikaur/build -type f -delete 2> /dev/null
-[ -d "/home/$(whoami)/.cache/pikaur/pkg" ]   && find /home/$(whoami)/.cache/pikaur/pkg   -type f -delete 2> /dev/null
+[ -d "/home/""$(whoami)""/.cache/pikaur/build" ] && find /home/"$(whoami)"/.cache/pikaur/build -type f -delete 2> /dev/null
+[ -d "/home/""$(whoami)""/.cache/pikaur/pkg" ]   && find /home/"$(whoami)"/.cache/pikaur/pkg   -type f -delete 2> /dev/null
 
 # Cleaning HOME folder
 [ -d "/home/$(whoami)/.thumbnails" ] && { find "/home/$(whoami)/.thumbnails" -type f -atime +7 -delete; find "/home/$(whoami)/.thumbnails" -empty -type d -atime +7 -delete ; }
