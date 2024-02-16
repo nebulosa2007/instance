69e7111ad89f24315e289eafa5070d988cc719f4
Nebulosa <nebulosa@yandex.ru>
2024-02-16 19:02:19 +0300

    Fix quotes

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,5 +4,5 @@
 ## sudo pacman -Syu --needed expac
 
 echo -e  "\033[1mFrom repository:\033[0m"
-expac -H M "%011m\t%-20n\t%10d" "$(comm -23 <(pacman -Qqen | sort) <((expac -l '\n' '%E' base-devel; expac -l '\n' '%E' base) | sort -u))"
-[ -n "$(pacman -Qm)"  ] && ( echo -e "\n\033[1mFrom AUR:\033[0m"; expac -H M "%011m\t%-20n\t%10d" "$(pacman -Qqm)" )
+expac -H M "%011m\t%-20n\t%10d" $(comm -23 <(pacman -Qqen | sort) <((expac -l '\n' '%E' base-devel; expac -l '\n' '%E' base) | sort -u))
+[ -n "$(pacman -Qm)"  ] && ( echo -e "\n\033[1mFrom AUR:\033[0m"; expac -H M "%011m\t%-20n\t%10d" $(pacman -Qqm) )
