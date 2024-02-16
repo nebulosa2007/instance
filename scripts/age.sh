66f80da75f910aa6693d9ff6a080652e533c6706
Nebulosa <nebulosa@yandex.ru>
2024-02-16 12:46:33 +0300

    Shellcheck respect

--- scripts/age.sh
+++ scripts/age.sh
@@ -6,15 +6,15 @@ DATE=$(/usr/bin/ls -ctl --time-style +"%Y-%m-%d" /etc | tail -1 | grep -Po "[0-9
 #or
 #DATE=$(head -1 /var/log/pacman.log | cut -c 2-11)
 
-LOCALE=$(echo $DATE | sed 's/-/ /g' | awk '{print $3"."$2"."$1}')
-DAYSBETWEEN=$(( ($(date -d $(date +%Y-%m-%d) +%s) - $(date -d $DATE +%s)) / 86400 ))
+LOCALE=$(echo "$DATE" | sed 's/-/ /g' | awk '{print $3"."$2"."$1}')
+DAYSBETWEEN=$(( ($(date -d "$(date +%Y-%m-%d)" +%s) - $(date -d "$DATE" +%s)) / 86400 ))
 
-YEARSCALC=$(( $DAYSBETWEEN / 365 ))
-MONTHSCALC=$(( ($DAYSBETWEEN - ($YEARSCALC * 365)) / 30 ))
-DAYSCALC=$(( $DAYSBETWEEN -($YEARSCALC * 365) - ($MONTHSCALC * 30) ))
+YEARSCALC=$(( "$DAYSBETWEEN" / 365 ))
+MONTHSCALC=$(( ("$DAYSBETWEEN" - ("$YEARSCALC" * 365)) / 30 ))
+DAYSCALC=$(( "$DAYSBETWEEN" -("$YEARSCALC" * 365) - ("$MONTHSCALC" * 30) ))
 
-[[ $YEARSCALC > 0  ]] && YEARS=$YEARSCALC"y "
-[[ $MONTHSCALC > 0 ]] && MONTHS=$MONTHSCALC"m "
-[[ $DAYSCALC > 0   ]] && DAYS=$DAYSCALC"d"
+[[ "$YEARSCALC" -gt 0  ]] && YEARS="$YEARSCALC""y "
+[[ "$MONTHSCALC" -gt 0 ]] && MONTHS="$MONTHSCALC""m "
+[[ "$DAYSCALC" -gt 0   ]] && DAYS="$DAYSCALC""d"
 
-echo "System age: "$YEARS$MONTHS$DAYS" (since $LOCALE)"
+echo "System age: ""$YEARS""$MONTHS""$DAYS"" (since $LOCALE)"
