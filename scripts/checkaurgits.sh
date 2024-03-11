3a7e5c4ae093bd29f50874cd6173a201984b322c
Nebulosa <nebulosa2007@yandex.ru>
2024-03-11 10:04:23 +0300

    Add check AUR git versions script

---
+++ scripts/checkaurgits.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+function checkaurgit (){
+
+    if [ "$1" == "repoctl" ] || [ "$2" == "repoctl" ]
+    then
+        mapfile -t gitpackages < <(repoctl list | grep "git")
+        mapfile -t gitpackageversions < <(repoctl list -v | grep "git" | cut -d " " -f2)
+    else
+        mapfile -t gitpackages < <(pacman -Qq | grep "\-git$")
+        mapfile -t gitpackageversions < <(pacman -Q | grep "\-git " | cut -d " " -f2)
+    fi
+
+    gitaurfolder=$HOME/.cache/aurgits
+    [ ! -d "$gitaurfolder" ] && mkdir "$gitaurfolder"
+
+    pushd "$gitaurfolder" > /dev/null || exit 1
+
+    for index in ${!gitpackages[*]}
+    do
+        gitpackage=${gitpackages[$index]}
+        gitpackageversion=${gitpackageversions[$index]}
+
+        [ -z "$lessinfo" ] && { echo && echo "Checking package: $gitpackage"; }
+        [ ! -d "$gitpackage" ] && git clone --quiet "https://aur.archlinux.org/$gitpackage.git" 2> /dev/null
+
+        builtin cd "$gitpackage" || exit 2
+
+        mapfile -t giturl < <(grep -Po '(?<=git\+)http.*' .SRCINFO | sed 's|#branch=| -b |')
+        [ ! -d "${gitpackage%-git}" ] && git clone --quiet ${giturl[*]} "${gitpackage%-git}" 2> /dev/null
+
+        builtin cd "${gitpackage%-git}" || exit 3
+        git pull --quiet
+        gitversion=$(set -o pipefail ; git describe --long --tags --abbrev=7 2>/dev/null | sed 's/\([^-]*-g\)/r\1/;s/-/./g;s/^v//' || printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
+
+        if [ "${gitpackageversion%-*}" != "$gitversion" ] && [[ ! "$gitpackageversion" =~ ${gitversion: -6} ]] 
+        then
+            echo "$gitpackage ${gitpackageversion%-*} -> $gitversion" 
+        else
+            [ -z "$lessinfo" ] && echo "Version ${gitpackageversion%-*} is up to date"
+        fi
+        builtin cd "$gitaurfolder" || exit 2
+    done
+    popd > /dev/null || exit 1
+}
+
+function print_help {
+    underline=$(tput smul)
+    nounderline=$(tput rmul)
+    echo "
+Check versions for -git packages in AUR. Support local installed packages or repoctl.
+
+${underline}Usage${nounderline}: ${0##*/} [OPTIONS] [repoctl]
+
+${underline}Options${nounderline}:
+       -h  Print help information
+       -q  Supress info, show info about updates only
+TODO:  -c  Clean cache folder after checking
+
+${underline}Modes${nounderline}:
+           Check installed packages. Default mode.
+  repoctl  Check packages at local repository
+"
+}
+
+getopts ":hq" option
+
+case "$option" in
+    h ) print_help;;
+    q ) lessinfo="y"; checkaurgit "$@";;
+    ? ) [ -n "$OPTARG" ] && { echo "Option not found, try -h" && exit 1; }
+        checkaurgit "$@";;
+esac
