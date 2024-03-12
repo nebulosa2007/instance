1dfc95c53b8596004b291c8aed3d1cd93f5fc47e
Nebulosa <nebulosa2007@yandex.ru>
2024-03-12 22:32:49 +0300

    Add b option

--- scripts/checkaurgits.sh
+++ scripts/checkaurgits.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+gitaurfolder=$HOME/.cache/aurgits
+
 function print_help {
     underline=$(tput smul)
     nounderline=$(tput rmul)
@@ -11,7 +13,7 @@ ${underline}Usage${nounderline}: ${0##*/} [OPTIONS] [repoctl]
 ${underline}Options${nounderline}:
        -h  Print help information
        -u  Print URL source for updates
-       -b  Build updates. Default option for makepkg is '-rs' or can be added, e.g. '-b rsCc'
+       -b  Build updates. Default option for makepkg is '-rfs' or can be added, e.g. '-b rsCc'
        -q  Supress info, show info about updates only
        -c  Clean cache folder after checking
 
@@ -21,6 +23,12 @@ ${underline}Modes${nounderline}:
 "
 }
 
+function cleanfolder (){
+    if [ -n "$clean" ]; then
+        [ -z "$lessinfo" ] && echo -e "\nCleaning cache..."
+        find "$gitaurfolder" -delete
+    fi
+}
 
 function checkaurgit (){
 
@@ -32,7 +40,6 @@ function checkaurgit (){
         mapfile -t gitpackageversions < <(pacman -Q | grep "\-git " | cut -d " " -f2)
     fi
 
-    gitaurfolder=$HOME/.cache/aurgits
     [ ! -d "$gitaurfolder" ] && mkdir "$gitaurfolder"
 
     pushd "$gitaurfolder" > /dev/null || exit 1
@@ -46,7 +53,7 @@ function checkaurgit (){
         [ ! -d "$gitpackage" ] && git clone --quiet "https://aur.archlinux.org/$gitpackage.git" 2> /dev/null
         builtin cd "$gitpackage" || exit 2
         mapfile -t giturl < <(grep -Po '(?<=git\+)http.*' .SRCINFO | sed 's|#branch=| -b |')
-        [ ! -d "${gitpackage%-git}" ] && git clone --quiet ${giturl[*]} "${gitpackage%-git}" 2> /dev/null
+        [ ! -d "${gitpackage%-git}" ] && git clone --quiet "${giturl[@]}" "${gitpackage%-git}" 2> /dev/null
 
         builtin cd "${gitpackage%-git}" || exit 3
         git pull --quiet
@@ -56,18 +63,16 @@ function checkaurgit (){
             git describe --long --tags --abbrev=7 2>/dev/null | sed 's/\([^-]*-g\)/r\1/;s/-/./g;s/^v//' \
          || printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
             )
+
         if [[ ! "$gitpackageversion" =~ ${gitversion: -6} ]]; then
             echo "$gitpackage ${gitpackageversion%-*} -> $gitversion"
             [ -n "$show_url" ] && [ -z "$lessinfo" ] && grep -Po '(?<=url = ).*' ../.SRCINFO
+            updpackages+=("$gitpackage")
         else
             [ -z "$lessinfo" ] && echo "Version ${gitpackageversion%-*} is up to date"
         fi
         builtin cd "$gitaurfolder" || exit 2
     done
-    if [ -n "$clean" ]; then
-        [ -z "$lessinfo" ] && echo -e "\nCleaning cache..."
-        find "$gitaurfolder" -delete
-    fi
     popd > /dev/null || exit 1
 }
 
@@ -75,21 +80,35 @@ while getopts ':hub:qc' option; do
     case "$option" in
          h ) print_help; exit 0;;
          u ) show_url="y";;
-         b ) build="y"; mkpgoptions="-$OPTARG";;
+         b ) mkpgoptions="-$OPTARG";;
          q ) lessinfo="y";;
          c ) clean="y";;
+         : ) mkpgoptions="-rfs";;
         \? ) [ -n "$OPTARG" ] && { echo "Option not found, try -h" && exit 1; }
     esac
 done
 shift $((OPTIND - 1))
 
-if [ "$1" == "repoctl" ]; then
+if [ "$1" == "repoctl" ] || [ "$mkpgoptions" == "-repoctl" ]; then
     [ -x /usr/bin/repoctl ] && checkaurgit repoctl || echo "Repoctl not installed. Exiting..."
 else
     checkaurgit
 fi
 
-if [ -n "$build" ]; then
-    [ -z "$mkpgoptions" ] && mkpgoptions="-rs"
-    echo -n "\nBuild with options $mkpgoptions"
+if [ -n "$mkpgoptions" ]; then
+    [ "$mkpgoptions" == "-repoctl" ] && mkpgoptions="-rfs"
+    for package in "${updpackages[@]}" ; do
+        pushd "$gitaurfolder/$package" > /dev/null || exit 1
+        echo -e "\nBuild $package with options $mkpgoptions"
+        if makepkg "$mkpgoptions" && [ -x /usr/bin/repoctl ]; then
+            read -n 1 -p "Add package to repository? [Y/n] " -r reply
+            [ "$reply" != "" ] && echo
+            if [ "$reply" = "${reply#[Nn]}" ]; then
+                find ! -name '*debug*' -name '*.pkg.tar.zst' -exec repoctl add {} \;
+            fi
+        fi
+        popd > /dev/null || exit 1
+    done
 fi
+
+cleanfolder
