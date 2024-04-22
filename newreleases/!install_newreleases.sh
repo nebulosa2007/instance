46bf85d7fee2a514e807cf5845438b84f798eb4f
Nebulosa <nebulosa2007@yandex.ru>
2024-04-22 19:24:15 +0300

    Add newreleases

---
+++ newreleases/!install_newreleases.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -S --needed git
+
+mkdir -p "/home/$(whoami)/.config/systemd/user"
+sudo ln -s "$PATHINSTANCE/newreleases/newreleases"         "/usr/bin/newreleases"
+     ln -s "$PATHINSTANCE/newreleases/newreleases.service" "/home/$(whoami)/.config/systemd/user/newreleases.service"
+     ln -s "$PATHINSTANCE/newreleases/newreleases.timer"   "/home/$(whoami)/.config/systemd/user/newreleases.timer"
+sudo loginctl enable-linger
+systemctl --user daemon-reload
+systemctl --user enable --now newreleases.timer
+systemctl status newreleases.timer
