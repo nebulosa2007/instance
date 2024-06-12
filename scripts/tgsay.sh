4c34fe07130e33f25445b884dd5ad77379b75115
Nebulosa <nebulosa2007@yandex.ru>
2024-06-12 13:13:43 +0300

    Add plus sign urlencoding

--- scripts/tgsay.sh
+++ scripts/tgsay.sh
@@ -10,7 +10,7 @@ if [ -z "$TG_BOT_CHAT_ID" ]; then
     exit 1
 fi
 
-MSG=$(echo "$@" | tr '\n' '[' | sed 's/ /%20/g;s/\[/%0A/g') #Urlencoding some simbols for curl
+MSG=$(echo "$@" | tr '\n' '[' | sed 's/ /%20/g;s/\[/%0A/g;s/+/%2B/g') #Urlencoding some simbols for curl
 
 #/usr/bin/wget -qO- "https://api.telegram.org/bot$TG_BOT_API_TOKEN/sendMessage?chat_id=$TG_BOT_CHAT_ID&parse_mode=html&text=$@" 2>&1
 /usr/bin/curl --no-progress-meter "https://api.telegram.org/bot$TG_BOT_API_TOKEN/sendMessage?chat_id=$TG_BOT_CHAT_ID&parse_mode=HTML&text=$MSG" 2>&1
