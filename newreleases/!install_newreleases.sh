d71825d1183e7dd3d78454c6ff557fa35a549e1b
Nebulosa <nebulosa2007@yandex.ru>
2025-02-16 20:13:18 +0300

    Move newrealease to local

--- newreleases/!install_newreleases.sh
+++ newreleases/!install_newreleases.sh
@@ -4,10 +4,10 @@
 sudo pacman -S --needed git
 
 mkdir -p "/home/$(whoami)/.config/systemd/user"
-sudo ln -s "$PATHINSTANCE/newreleases/newreleases" "/usr/bin/newreleases"
+sudo ln -s "$PATHINSTANCE/newreleases/newreleases" "/usr/local/bin/newreleases"
 ln -s "$PATHINSTANCE/newreleases/newreleases.service" "/home/$(whoami)/.config/systemd/user/newreleases.service"
 ln -s "$PATHINSTANCE/newreleases/newreleases.timer" "/home/$(whoami)/.config/systemd/user/newreleases.timer"
 sudo loginctl enable-linger
 systemctl --user daemon-reload
 systemctl --user enable --now newreleases.timer
-systemctl status newreleases.timer
+systemctl --user status newreleases.timer
