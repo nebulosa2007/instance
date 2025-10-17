47dee923993c24a54e0b556a08b3ee43563b7cb2
Nebulosa <nebulosa2007@yandex.ru>
2025-10-18 00:37:22 +0300

    Set preview to false by default

--- scripts/tgsay.sh
+++ scripts/tgsay.sh
@@ -9,7 +9,8 @@ source "$PATHINSTANCE"/scripts/sensitive.sh 2>/dev/null
 
 : "${TG_BOT_CHAT_ID:?Please, define TG_BOT_CHAT_ID and TG_BOT_API_TOKEN first! To get TG_BOT_CHAT_ID, run the following command and look for the \"chat\":{\"id\":xxxxxxx string: curl -s https://api.telegram.org/bot$TG_BOT_API_TOKEN/getUpdates}"
 
-MSG=$(echo "$@" | tr '\n' '[' | sed 's/ /%20/g;s/\[/%0A/g;s/\]/%5D/g;s/+/%2B/g') #Urlencoding some symbols for curl
+MSG=$(echo "$1" | tr '\n' '[' | sed 's/ /%20/g;s/\[/%0A/g;s/\]/%5D/g;s/+/%2B/g') #Urlencoding some symbols for curl
+DISABLE_PREVIEW="${2:-true}"
 
 #/usr/bin/wget -qO- "https://api.telegram.org/bot$TG_BOT_API_TOKEN/sendMessage?chat_id=$TG_BOT_CHAT_ID&parse_mode=html&text=$@" 2>&1
-/usr/bin/curl --no-progress-meter "https://api.telegram.org/bot$TG_BOT_API_TOKEN/sendMessage?chat_id=$TG_BOT_CHAT_ID&parse_mode=HTML&text=$MSG" 2>&1
+/usr/bin/curl --no-progress-meter "https://api.telegram.org/bot$TG_BOT_API_TOKEN/sendMessage?chat_id=$TG_BOT_CHAT_ID&parse_mode=HTML&disable_web_page_preview=$DISABLE_PREVIEW&text=$MSG" 2>&1
