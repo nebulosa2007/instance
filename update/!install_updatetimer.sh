65718770a7b2e05fd08cffea1c0cbce15b9a61fa
Nebulosa <nebulosa2007@yandex.ru>
2025-11-15 19:14:29 +0300

    Remove network-online.target dependency

--- update/!install_updatetimer.sh
+++ update/!install_updatetimer.sh
@@ -10,6 +10,6 @@ sudo cp "$PATHINSTANCE"/update/update.{service,timer} -t /etc/systemd/system
 sudo systemctl daemon-reload
 
 #Check if any "wait-online-service" is working. Credits: https://wiki.archlinux.org/title/Systemd-networkd#systemd-networkd-wait-online
-systemctl is-enabled NetworkManager-wait-online.service systemd-networkd-wait-online.service
+#systemctl is-enabled NetworkManager-wait-online.service systemd-networkd-wait-online.service
 
 sudo systemctl enable --now update.timer
