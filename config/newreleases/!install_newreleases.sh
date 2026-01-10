8d06bb498f8e655ad9b67ed933740cfbb98cac6f
Nebulosa <nebulosa2007@yandex.ru>
2026-01-11 01:21:10 +0300

    Fix instructions

--- config/newreleases/!install_newreleases.sh
+++ config/newreleases/!install_newreleases.sh
@@ -4,9 +4,9 @@
 sudo pacman -S --needed git
 
 mkdir -p "/home/$(whoami)/.config/systemd/user"
-sudo ln -s "$PATHINSTANCE/newreleases/newreleases" "/usr/local/bin/newreleases"
-ln -s "$PATHINSTANCE/newreleases/newreleases.service" "/home/$(whoami)/.config/systemd/user/newreleases.service"
-ln -s "$PATHINSTANCE/newreleases/newreleases.timer" "/home/$(whoami)/.config/systemd/user/newreleases.timer"
+sudo ln -sf "$PATHINSTANCE/config/newreleases/newreleases" "/usr/local/bin/newreleases"
+ln -sf "$PATHINSTANCE/config/newreleases/newreleases.service" "/home/$(whoami)/.config/systemd/user/newreleases.service"
+ln -sf "$PATHINSTANCE/config/newreleases/newreleases.timer" "/home/$(whoami)/.config/systemd/user/newreleases.timer"
 sudo loginctl enable-linger
 systemctl --user daemon-reload
 systemctl --user enable --now newreleases.timer
--- etc/update/!install_updatetimer.sh
+++ etc/update/!install_updatetimer.sh
@@ -4,9 +4,9 @@
 #Updates module of Instance project
 
 #IMPORTANT: Only for one partition systems. Use cp instead ln below:
-sudo ln -sf "$PATHINSTANCE"/update/updpkgs.sh /usr/local/bin/updpkgs
+sudo ln -sf "$PATHINSTANCE"/etc/update/updpkgs.sh /usr/local/bin/updpkgs
 
-sudo cp "$PATHINSTANCE"/update/update.{service,timer} -t /etc/systemd/system
+sudo cp "$PATHINSTANCE"/etc/update/update.{service,timer} -t /etc/systemd/system
 sudo systemctl daemon-reload
 
 #Check if any "wait-online-service" is working. Credits: https://wiki.archlinux.org/title/Systemd-networkd#systemd-networkd-wait-online
