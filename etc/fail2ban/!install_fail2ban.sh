61d98ea35bfaa30ffdab812d2cc205189bf6bf52
Nebulosa <nebulosa2007@yandex.ru>
2026-07-23 11:51:06 +0300

    Update fail2ban conf files

--- etc/fail2ban/!install_fail2ban.sh
+++ etc/fail2ban/!install_fail2ban.sh
@@ -4,10 +4,10 @@
 #fail2ban module of Instance project
 
 sudo pacman -Syu fail2ban
-sudo cp "$PATHINSTANCE"/etc/fail2ban/{telegram-geo,nftables}.local -t /etc/fail2ban/action.d
-sudo cp "$PATHINSTANCE"/etc/fail2ban/{recidive,nginx-444}.local    -t /etc/fail2ban/filter.d
-sudo cp "$PATHINSTANCE"/etc/fail2ban/jail.local                    -t /etc/fail2ban
-sudo cp "$PATHINSTANCE"/etc/fail2ban/fail2bantgsay                 -t /usr/local/bin
+sudo cp "$PATHINSTANCE"/etc/fail2ban/{telegram-geo,nftables,ufw}.local -t /etc/fail2ban/action.d
+sudo cp "$PATHINSTANCE"/etc/fail2ban/{recidive,nginx-444}.local        -t /etc/fail2ban/filter.d
+sudo cp "$PATHINSTANCE"/etc/fail2ban/jail.local                        -t /etc/fail2ban
+sudo cp "$PATHINSTANCE"/etc/fail2ban/f2b-telegram.sh                   -t /usr/local/bin
 
 sudo fail2ban-client -d
 sudo systemctl enable --now fail2ban
